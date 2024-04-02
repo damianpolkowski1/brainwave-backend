7a9058ac9a7a050f3994b7d8fa72cb7d947b37df
Damian <damianpolkowski13@gmail.com>
2024-04-02 19:19:27 +0200

    Created necessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './modules/category/category.module';
+import { QuestionModule } from './modules/question/question.module';
+import { ScoreModule } from './modules/score/score.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule, QuestionModule, ScoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
